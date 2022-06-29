a586f949dbf4e324e267d7b0d53418cc3a7192e3
Miguel Ramos <miguel.ramos@websublime.dev>
2022-06-29 14:04:52 +0100

    Upload artifact index.d.ts at 2022.06.29-13:04:51

---
+++ assets/wbs-loader@0.0.0-SNAPSHOT/index.d.ts
@@ -0,0 +1,42 @@
+import { ComponentElement } from '@websublime/wbs-ui-common';
+import { ComponentMetadata } from '@websublime/wbs-ui-common';
+import { Ref } from '@websublime/wbs-ui-common';
+import { TemplateResult } from 'lit-html';
+import { WebComponentOptions } from '@websublime/wbs-ui-common';
+
+/**
+ * @public
+ */
+declare const registerComponent: (options?: WsLoaderOptions, name?: string) => new () => WsLoader;
+export default registerComponent;
+
+/**
+ * Loader component element
+ */
+export declare class WsLoader extends ComponentElement {
+    static shadowRootOptions: {
+        delegatesFocus: boolean;
+        mode: ShadowRootMode;
+        slotAssignment?: SlotAssignmentMode | undefined;
+    };
+    elRef: Ref<HTMLHeadingElement>;
+    name: string;
+    scope: string;
+    /**
+     * Element id
+     */
+    private identifier;
+    get element(): {
+        element: Ref<HTMLHeadingElement>;
+        registry: ComponentMetadata;
+    };
+    constructor();
+    connectedCallback(): void;
+    render(): TemplateResult<1>;
+}
+
+declare interface WsLoaderOptions extends WebComponentOptions {
+    className?: string[];
+}
+
+export { }

0770304fe43bc7f8f441401a166a849b1c7b6957
Miguel Ramos <miguel.ramos@websublime.dev>
2022-06-30 01:13:50 +0100

    Upload artifact wbs-ui-header.es.js at 2022.06.30-00:13:50

--- assets/wbs-ui-header@0.0.2-SNAPSHOT/wbs-ui-header.es.js
+++ assets/wbs-ui-header@0.0.2-SNAPSHOT/wbs-ui-header.es.js
@@ -1,5 +1,488 @@
-import { ComponentElement, createRef, uniqueID, ref, defineWebComponent } from "@websublime/wbs-ui-common";
 import { LitElement, html } from "lit";
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const i$2 = (i2, e2) => e2.kind === "method" && e2.descriptor && !("value" in e2.descriptor) ? { ...e2, finisher(n2) {
+  n2.createProperty(e2.key, i2);
+} } : { kind: "field", key: Symbol(), placement: "own", descriptor: {}, originalKey: e2.key, initializer() {
+  typeof e2.initializer == "function" && (this[e2.key] = e2.initializer.call(this));
+}, finisher(n2) {
+  n2.createProperty(e2.key, i2);
+} };
+function e$4(e2) {
+  return (n2, t2) => t2 !== void 0 ? ((i2, e3, n3) => {
+    e3.constructor.createProperty(n3, i2);
+  })(e2, n2, t2) : i$2(e2, n2);
+}
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+var n$3;
+((n$3 = window.HTMLSlotElement) === null || n$3 === void 0 ? void 0 : n$3.prototype.assignedElements) != null ? (o2, n2) => o2.assignedElements(n2) : (o2, n2) => o2.assignedNodes(n2).filter((o3) => o3.nodeType === Node.ELEMENT_NODE);
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+var t$1;
+const i$1 = globalThis.trustedTypes, s = i$1 ? i$1.createPolicy("lit-html", { createHTML: (t2) => t2 }) : void 0, e$3 = `lit$${(Math.random() + "").slice(9)}$`, o$2 = "?" + e$3, n$2 = `<${o$2}>`, l$1 = document, h$2 = (t2 = "") => l$1.createComment(t2), r$2 = (t2) => t2 === null || typeof t2 != "object" && typeof t2 != "function", d$1 = Array.isArray, u = (t2) => {
+  var i2;
+  return d$1(t2) || typeof ((i2 = t2) === null || i2 === void 0 ? void 0 : i2[Symbol.iterator]) == "function";
+}, c = /<(?:(!--|\/[^a-zA-Z])|(\/?[a-zA-Z][^>\s]*)|(\/?$))/g, v = /-->/g, a = />/g, f = />|[ 	\n\r](?:([^\s"'>=/]+)([ 	\n\r]*=[ 	\n\r]*(?:[^ 	\n\r"'`<>=]|("|')|))|$)/g, _ = /'/g, m = /"/g, g = /^(?:script|style|textarea|title)$/i, b = Symbol.for("lit-noChange"), w = Symbol.for("lit-nothing"), T = /* @__PURE__ */ new WeakMap(), A = l$1.createTreeWalker(l$1, 129, null, false), C = (t2, i2) => {
+  const o2 = t2.length - 1, l2 = [];
+  let h2, r2 = i2 === 2 ? "<svg>" : "", d2 = c;
+  for (let i3 = 0; i3 < o2; i3++) {
+    const s2 = t2[i3];
+    let o3, u3, p = -1, $ = 0;
+    for (; $ < s2.length && (d2.lastIndex = $, u3 = d2.exec(s2), u3 !== null); )
+      $ = d2.lastIndex, d2 === c ? u3[1] === "!--" ? d2 = v : u3[1] !== void 0 ? d2 = a : u3[2] !== void 0 ? (g.test(u3[2]) && (h2 = RegExp("</" + u3[2], "g")), d2 = f) : u3[3] !== void 0 && (d2 = f) : d2 === f ? u3[0] === ">" ? (d2 = h2 != null ? h2 : c, p = -1) : u3[1] === void 0 ? p = -2 : (p = d2.lastIndex - u3[2].length, o3 = u3[1], d2 = u3[3] === void 0 ? f : u3[3] === '"' ? m : _) : d2 === m || d2 === _ ? d2 = f : d2 === v || d2 === a ? d2 = c : (d2 = f, h2 = void 0);
+    const y = d2 === f && t2[i3 + 1].startsWith("/>") ? " " : "";
+    r2 += d2 === c ? s2 + n$2 : p >= 0 ? (l2.push(o3), s2.slice(0, p) + "$lit$" + s2.slice(p) + e$3 + y) : s2 + e$3 + (p === -2 ? (l2.push(void 0), i3) : y);
+  }
+  const u2 = r2 + (t2[o2] || "<?>") + (i2 === 2 ? "</svg>" : "");
+  if (!Array.isArray(t2) || !t2.hasOwnProperty("raw"))
+    throw Error("invalid template strings array");
+  return [s !== void 0 ? s.createHTML(u2) : u2, l2];
+};
+class E {
+  constructor({ strings: t2, _$litType$: s2 }, n2) {
+    let l2;
+    this.parts = [];
+    let r2 = 0, d2 = 0;
+    const u2 = t2.length - 1, c2 = this.parts, [v2, a2] = C(t2, s2);
+    if (this.el = E.createElement(v2, n2), A.currentNode = this.el.content, s2 === 2) {
+      const t3 = this.el.content, i2 = t3.firstChild;
+      i2.remove(), t3.append(...i2.childNodes);
+    }
+    for (; (l2 = A.nextNode()) !== null && c2.length < u2; ) {
+      if (l2.nodeType === 1) {
+        if (l2.hasAttributes()) {
+          const t3 = [];
+          for (const i2 of l2.getAttributeNames())
+            if (i2.endsWith("$lit$") || i2.startsWith(e$3)) {
+              const s3 = a2[d2++];
+              if (t3.push(i2), s3 !== void 0) {
+                const t4 = l2.getAttribute(s3.toLowerCase() + "$lit$").split(e$3), i3 = /([.?@])?(.*)/.exec(s3);
+                c2.push({ type: 1, index: r2, name: i3[2], strings: t4, ctor: i3[1] === "." ? M : i3[1] === "?" ? H : i3[1] === "@" ? I : S });
+              } else
+                c2.push({ type: 6, index: r2 });
+            }
+          for (const i2 of t3)
+            l2.removeAttribute(i2);
+        }
+        if (g.test(l2.tagName)) {
+          const t3 = l2.textContent.split(e$3), s3 = t3.length - 1;
+          if (s3 > 0) {
+            l2.textContent = i$1 ? i$1.emptyScript : "";
+            for (let i2 = 0; i2 < s3; i2++)
+              l2.append(t3[i2], h$2()), A.nextNode(), c2.push({ type: 2, index: ++r2 });
+            l2.append(t3[s3], h$2());
+          }
+        }
+      } else if (l2.nodeType === 8)
+        if (l2.data === o$2)
+          c2.push({ type: 2, index: r2 });
+        else {
+          let t3 = -1;
+          for (; (t3 = l2.data.indexOf(e$3, t3 + 1)) !== -1; )
+            c2.push({ type: 7, index: r2 }), t3 += e$3.length - 1;
+        }
+      r2++;
+    }
+  }
+  static createElement(t2, i2) {
+    const s2 = l$1.createElement("template");
+    return s2.innerHTML = t2, s2;
+  }
+}
+function P(t2, i2, s2 = t2, e2) {
+  var o2, n2, l2, h2;
+  if (i2 === b)
+    return i2;
+  let d2 = e2 !== void 0 ? (o2 = s2._$Cl) === null || o2 === void 0 ? void 0 : o2[e2] : s2._$Cu;
+  const u2 = r$2(i2) ? void 0 : i2._$litDirective$;
+  return (d2 == null ? void 0 : d2.constructor) !== u2 && ((n2 = d2 == null ? void 0 : d2._$AO) === null || n2 === void 0 || n2.call(d2, false), u2 === void 0 ? d2 = void 0 : (d2 = new u2(t2), d2._$AT(t2, s2, e2)), e2 !== void 0 ? ((l2 = (h2 = s2)._$Cl) !== null && l2 !== void 0 ? l2 : h2._$Cl = [])[e2] = d2 : s2._$Cu = d2), d2 !== void 0 && (i2 = P(t2, d2._$AS(t2, i2.values), d2, e2)), i2;
+}
+class V {
+  constructor(t2, i2) {
+    this.v = [], this._$AN = void 0, this._$AD = t2, this._$AM = i2;
+  }
+  get parentNode() {
+    return this._$AM.parentNode;
+  }
+  get _$AU() {
+    return this._$AM._$AU;
+  }
+  p(t2) {
+    var i2;
+    const { el: { content: s2 }, parts: e2 } = this._$AD, o2 = ((i2 = t2 == null ? void 0 : t2.creationScope) !== null && i2 !== void 0 ? i2 : l$1).importNode(s2, true);
+    A.currentNode = o2;
+    let n2 = A.nextNode(), h2 = 0, r2 = 0, d2 = e2[0];
+    for (; d2 !== void 0; ) {
+      if (h2 === d2.index) {
+        let i3;
+        d2.type === 2 ? i3 = new N(n2, n2.nextSibling, this, t2) : d2.type === 1 ? i3 = new d2.ctor(n2, d2.name, d2.strings, this, t2) : d2.type === 6 && (i3 = new L(n2, this, t2)), this.v.push(i3), d2 = e2[++r2];
+      }
+      h2 !== (d2 == null ? void 0 : d2.index) && (n2 = A.nextNode(), h2++);
+    }
+    return o2;
+  }
+  m(t2) {
+    let i2 = 0;
+    for (const s2 of this.v)
+      s2 !== void 0 && (s2.strings !== void 0 ? (s2._$AI(t2, s2, i2), i2 += s2.strings.length - 2) : s2._$AI(t2[i2])), i2++;
+  }
+}
+class N {
+  constructor(t2, i2, s2, e2) {
+    var o2;
+    this.type = 2, this._$AH = w, this._$AN = void 0, this._$AA = t2, this._$AB = i2, this._$AM = s2, this.options = e2, this._$Cg = (o2 = e2 == null ? void 0 : e2.isConnected) === null || o2 === void 0 || o2;
+  }
+  get _$AU() {
+    var t2, i2;
+    return (i2 = (t2 = this._$AM) === null || t2 === void 0 ? void 0 : t2._$AU) !== null && i2 !== void 0 ? i2 : this._$Cg;
+  }
+  get parentNode() {
+    let t2 = this._$AA.parentNode;
+    const i2 = this._$AM;
+    return i2 !== void 0 && t2.nodeType === 11 && (t2 = i2.parentNode), t2;
+  }
+  get startNode() {
+    return this._$AA;
+  }
+  get endNode() {
+    return this._$AB;
+  }
+  _$AI(t2, i2 = this) {
+    t2 = P(this, t2, i2), r$2(t2) ? t2 === w || t2 == null || t2 === "" ? (this._$AH !== w && this._$AR(), this._$AH = w) : t2 !== this._$AH && t2 !== b && this.$(t2) : t2._$litType$ !== void 0 ? this.T(t2) : t2.nodeType !== void 0 ? this.k(t2) : u(t2) ? this.S(t2) : this.$(t2);
+  }
+  M(t2, i2 = this._$AB) {
+    return this._$AA.parentNode.insertBefore(t2, i2);
+  }
+  k(t2) {
+    this._$AH !== t2 && (this._$AR(), this._$AH = this.M(t2));
+  }
+  $(t2) {
+    this._$AH !== w && r$2(this._$AH) ? this._$AA.nextSibling.data = t2 : this.k(l$1.createTextNode(t2)), this._$AH = t2;
+  }
+  T(t2) {
+    var i2;
+    const { values: s2, _$litType$: e2 } = t2, o2 = typeof e2 == "number" ? this._$AC(t2) : (e2.el === void 0 && (e2.el = E.createElement(e2.h, this.options)), e2);
+    if (((i2 = this._$AH) === null || i2 === void 0 ? void 0 : i2._$AD) === o2)
+      this._$AH.m(s2);
+    else {
+      const t3 = new V(o2, this), i3 = t3.p(this.options);
+      t3.m(s2), this.k(i3), this._$AH = t3;
+    }
+  }
+  _$AC(t2) {
+    let i2 = T.get(t2.strings);
+    return i2 === void 0 && T.set(t2.strings, i2 = new E(t2)), i2;
+  }
+  S(t2) {
+    d$1(this._$AH) || (this._$AH = [], this._$AR());
+    const i2 = this._$AH;
+    let s2, e2 = 0;
+    for (const o2 of t2)
+      e2 === i2.length ? i2.push(s2 = new N(this.M(h$2()), this.M(h$2()), this, this.options)) : s2 = i2[e2], s2._$AI(o2), e2++;
+    e2 < i2.length && (this._$AR(s2 && s2._$AB.nextSibling, e2), i2.length = e2);
+  }
+  _$AR(t2 = this._$AA.nextSibling, i2) {
+    var s2;
+    for ((s2 = this._$AP) === null || s2 === void 0 || s2.call(this, false, true, i2); t2 && t2 !== this._$AB; ) {
+      const i3 = t2.nextSibling;
+      t2.remove(), t2 = i3;
+    }
+  }
+  setConnected(t2) {
+    var i2;
+    this._$AM === void 0 && (this._$Cg = t2, (i2 = this._$AP) === null || i2 === void 0 || i2.call(this, t2));
+  }
+}
+class S {
+  constructor(t2, i2, s2, e2, o2) {
+    this.type = 1, this._$AH = w, this._$AN = void 0, this.element = t2, this.name = i2, this._$AM = e2, this.options = o2, s2.length > 2 || s2[0] !== "" || s2[1] !== "" ? (this._$AH = Array(s2.length - 1).fill(new String()), this.strings = s2) : this._$AH = w;
+  }
+  get tagName() {
+    return this.element.tagName;
+  }
+  get _$AU() {
+    return this._$AM._$AU;
+  }
+  _$AI(t2, i2 = this, s2, e2) {
+    const o2 = this.strings;
+    let n2 = false;
+    if (o2 === void 0)
+      t2 = P(this, t2, i2, 0), n2 = !r$2(t2) || t2 !== this._$AH && t2 !== b, n2 && (this._$AH = t2);
+    else {
+      const e3 = t2;
+      let l2, h2;
+      for (t2 = o2[0], l2 = 0; l2 < o2.length - 1; l2++)
+        h2 = P(this, e3[s2 + l2], i2, l2), h2 === b && (h2 = this._$AH[l2]), n2 || (n2 = !r$2(h2) || h2 !== this._$AH[l2]), h2 === w ? t2 = w : t2 !== w && (t2 += (h2 != null ? h2 : "") + o2[l2 + 1]), this._$AH[l2] = h2;
+    }
+    n2 && !e2 && this.C(t2);
+  }
+  C(t2) {
+    t2 === w ? this.element.removeAttribute(this.name) : this.element.setAttribute(this.name, t2 != null ? t2 : "");
+  }
+}
+class M extends S {
+  constructor() {
+    super(...arguments), this.type = 3;
+  }
+  C(t2) {
+    this.element[this.name] = t2 === w ? void 0 : t2;
+  }
+}
+const k = i$1 ? i$1.emptyScript : "";
+class H extends S {
+  constructor() {
+    super(...arguments), this.type = 4;
+  }
+  C(t2) {
+    t2 && t2 !== w ? this.element.setAttribute(this.name, k) : this.element.removeAttribute(this.name);
+  }
+}
+class I extends S {
+  constructor(t2, i2, s2, e2, o2) {
+    super(t2, i2, s2, e2, o2), this.type = 5;
+  }
+  _$AI(t2, i2 = this) {
+    var s2;
+    if ((t2 = (s2 = P(this, t2, i2, 0)) !== null && s2 !== void 0 ? s2 : w) === b)
+      return;
+    const e2 = this._$AH, o2 = t2 === w && e2 !== w || t2.capture !== e2.capture || t2.once !== e2.once || t2.passive !== e2.passive, n2 = t2 !== w && (e2 === w || o2);
+    o2 && this.element.removeEventListener(this.name, this, e2), n2 && this.element.addEventListener(this.name, this, t2), this._$AH = t2;
+  }
+  handleEvent(t2) {
+    var i2, s2;
+    typeof this._$AH == "function" ? this._$AH.call((s2 = (i2 = this.options) === null || i2 === void 0 ? void 0 : i2.host) !== null && s2 !== void 0 ? s2 : this.element, t2) : this._$AH.handleEvent(t2);
+  }
+}
+class L {
+  constructor(t2, i2, s2) {
+    this.element = t2, this.type = 6, this._$AN = void 0, this._$AM = i2, this.options = s2;
+  }
+  get _$AU() {
+    return this._$AM._$AU;
+  }
+  _$AI(t2) {
+    P(this, t2);
+  }
+}
+const z = window.litHtmlPolyfillSupport;
+z == null || z(E, N), ((t$1 = globalThis.litHtmlVersions) !== null && t$1 !== void 0 ? t$1 : globalThis.litHtmlVersions = []).push("2.2.6");
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const t = { ATTRIBUTE: 1, CHILD: 2, PROPERTY: 3, BOOLEAN_ATTRIBUTE: 4, EVENT: 5, ELEMENT: 6 }, e$2 = (t2) => (...e2) => ({ _$litDirective$: t2, values: e2 });
+class i {
+  constructor(t2) {
+  }
+  get _$AU() {
+    return this._$AM._$AU;
+  }
+  _$AT(t2, e2, i2) {
+    this._$Ct = t2, this._$AM = e2, this._$Ci = i2;
+  }
+  _$AS(t2, e2) {
+    return this.update(t2, e2);
+  }
+  update(t2, e2) {
+    return this.render(...e2);
+  }
+}
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const r$1 = (o2) => o2.strings === void 0;
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const e$1 = (i2, t2) => {
+  var s2, o2;
+  const n2 = i2._$AN;
+  if (n2 === void 0)
+    return false;
+  for (const i3 of n2)
+    (o2 = (s2 = i3)._$AO) === null || o2 === void 0 || o2.call(s2, t2, false), e$1(i3, t2);
+  return true;
+}, o$1 = (i2) => {
+  let t2, s2;
+  do {
+    if ((t2 = i2._$AM) === void 0)
+      break;
+    s2 = t2._$AN, s2.delete(i2), i2 = t2;
+  } while ((s2 == null ? void 0 : s2.size) === 0);
+}, n$1 = (i2) => {
+  for (let t2; t2 = i2._$AM; i2 = t2) {
+    let s2 = t2._$AN;
+    if (s2 === void 0)
+      t2._$AN = s2 = /* @__PURE__ */ new Set();
+    else if (s2.has(i2))
+      break;
+    s2.add(i2), l(t2);
+  }
+};
+function r(i2) {
+  this._$AN !== void 0 ? (o$1(this), this._$AM = i2, n$1(this)) : this._$AM = i2;
+}
+function h$1(i2, t2 = false, s2 = 0) {
+  const n2 = this._$AH, r2 = this._$AN;
+  if (r2 !== void 0 && r2.size !== 0)
+    if (t2)
+      if (Array.isArray(n2))
+        for (let i3 = s2; i3 < n2.length; i3++)
+          e$1(n2[i3], false), o$1(n2[i3]);
+      else
+        n2 != null && (e$1(n2, false), o$1(n2));
+    else
+      e$1(this, i2);
+}
+const l = (i2) => {
+  var t$12, e2, o2, n2;
+  i2.type == t.CHILD && ((t$12 = (o2 = i2)._$AP) !== null && t$12 !== void 0 || (o2._$AP = h$1), (e2 = (n2 = i2)._$AQ) !== null && e2 !== void 0 || (n2._$AQ = r));
+};
+class d extends i {
+  constructor() {
+    super(...arguments), this._$AN = void 0;
+  }
+  _$AT(i2, t2, s2) {
+    super._$AT(i2, t2, s2), n$1(this), this.isConnected = i2._$AU;
+  }
+  _$AO(i2, t2 = true) {
+    var s2, n2;
+    i2 !== this.isConnected && (this.isConnected = i2, i2 ? (s2 = this.reconnected) === null || s2 === void 0 || s2.call(this) : (n2 = this.disconnected) === null || n2 === void 0 || n2.call(this)), t2 && (e$1(this, i2), o$1(this));
+  }
+  setValue(t2) {
+    if (r$1(this._$Ct))
+      this._$Ct._$AI(t2, this);
+    else {
+      const i2 = [...this._$Ct._$AH];
+      i2[this._$Ci] = t2, this._$Ct._$AI(i2, this, 0);
+    }
+  }
+  disconnected() {
+  }
+  reconnected() {
+  }
+}
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+const e = () => new o();
+class o {
+}
+const h = /* @__PURE__ */ new WeakMap(), n = e$2(class extends d {
+  render(t2) {
+    return w;
+  }
+  update(t2, [s2]) {
+    var e2;
+    const o2 = s2 !== this.U;
+    return o2 && this.U !== void 0 && this.ot(void 0), (o2 || this.rt !== this.lt) && (this.U = s2, this.ht = (e2 = t2.options) === null || e2 === void 0 ? void 0 : e2.host, this.ot(this.lt = t2.element)), w;
+  }
+  ot(i2) {
+    var t2;
+    if (typeof this.U == "function") {
+      const s2 = (t2 = this.ht) !== null && t2 !== void 0 ? t2 : globalThis;
+      let e2 = h.get(s2);
+      e2 === void 0 && (e2 = /* @__PURE__ */ new WeakMap(), h.set(s2, e2)), e2.get(this.U) !== void 0 && this.U.call(this.ht, void 0), e2.set(this.U, i2), i2 !== void 0 && this.U.call(this.ht, i2);
+    } else
+      this.U.value = i2;
+  }
+  get rt() {
+    var i2, t2, s2;
+    return typeof this.U == "function" ? (t2 = h.get((i2 = this.ht) !== null && i2 !== void 0 ? i2 : globalThis)) === null || t2 === void 0 ? void 0 : t2.get(this.U) : (s2 = this.U) === null || s2 === void 0 ? void 0 : s2.value;
+  }
+  disconnected() {
+    this.rt === this.lt && this.ot(void 0);
+  }
+  reconnected() {
+    this.ot(this.lt);
+  }
+});
+const uniqueID = () => {
+  const head = Date.now().toString(36);
+  const tail = Math.random().toString(36).substr(2);
+  return head + tail;
+};
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __decorateClass = (decorators, target, key, kind) => {
+  var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+  for (var i2 = decorators.length - 1, decorator; i2 >= 0; i2--)
+    if (decorator = decorators[i2])
+      result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+  if (kind && result)
+    __defProp(target, key, result);
+  return result;
+};
+function ComponentMixin(constructor) {
+  class SuperElement extends constructor {
+    constructor() {
+      super(...arguments);
+      this.dir = "ltr";
+      this.inspect = false;
+    }
+    set options(options) {
+      const oldValue = {
+        ...this.settings,
+        ...this._options
+      };
+      this._options = {
+        ...this.settings,
+        ...options
+      };
+      this.requestUpdate("options", oldValue);
+    }
+    get options() {
+      var _a;
+      return (_a = this._options) != null ? _a : this.settings;
+    }
+    get isLTR() {
+      return this.dir === "ltr";
+    }
+  }
+  __decorateClass([
+    e$4({ type: Object })
+  ], SuperElement.prototype, "options", 1);
+  __decorateClass([
+    e$4({ reflect: true })
+  ], SuperElement.prototype, "dir", 2);
+  __decorateClass([
+    e$4({ reflect: true, type: Boolean })
+  ], SuperElement.prototype, "inspect", 2);
+  return SuperElement;
+}
+class ComponentElement extends ComponentMixin(LitElement) {
+  constructor(registry) {
+    super();
+    this.registry = Object.freeze(registry);
+    this.dataset.qa = registry.qa;
+  }
+}
+function defineWebComponent(name, component, options = {}) {
+  Object.defineProperty(component.prototype, "settings", {
+    enumerable: true,
+    value: options,
+    writable: true
+  });
+  window.customElements.define(name, component);
+  return component;
+}
 const version = "0.0.2";
 const metadata = {
   description: "Header container component",
@@ -12,7 +495,7 @@ const metadata = {
 class WsHeader extends ComponentElement {
   constructor() {
     super(metadata);
-    this.elRef = createRef();
+    this.elRef = e();
     this.identifier = uniqueID();
     const classes = this.options.className;
     this.classList.add(...classes);
@@ -23,7 +506,7 @@ class WsHeader extends ComponentElement {
   render() {
     return html`
       <ws-inspector .el=${this.element} .inspect=${this.inspect}></ws-inspector>
-      <header id=${this.identifier} ${ref(this.elRef)}>
+      <header id=${this.identifier} ${n(this.elRef)}>
         <slot name="slogan"></slot>
         <slot name="menu"></slot>
         <slot></slot>
